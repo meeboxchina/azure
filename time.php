e2064f55a1e8183180cc1143bd2aa5615e092e92
Sunny Sun <sunyu.cn@gmail.com>
2015-11-03 17:06:02 +0800

    add time page

--- time.php
+++ time.php
@@ -1,9 +1,9 @@
 <?php
 	echo date("Y-m-d H:i-s",time());
-	echo "<br />"
+	echo "<br />";
 	echo $_SERVER["REMOTE_ADDR"];
-	echo "<br />"
+	echo "<br />";
 	echo $_SERVER["HTTP_CLIENT_IP"];
-	echo "<br />"
+	echo "<br />";
 	echo $_SERVER["HTTP_X_FORWARDED_FOR"];	
 ?>
